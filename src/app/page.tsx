902dcec4f3cc0f6f892fe5b009c2f977686511bf
Matthew Yau <myau0005@student.monash.edu>
2025-03-27 22:42:02 +1100

    metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,3 +5,16 @@ import Portfolio from "@/components/Portfolio";
 export default function HomePage() {
   return <Portfolio />;
 }
+
+export const metadata = {
+  title: "Portfolio Website",
+  description: "Created by Matthew Yau.",
+  openGraph: {
+    title: "Portfolio Website",
+    description: "Created by Matthew Yau.",
+    url: "https://matthewyau.com",
+    siteName: "Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+};
