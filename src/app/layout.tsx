12c7691a69ee0af118d7fadc3440b71dbedd3e09
Matthew Yau <myau0005@student.monash.edu>
2025-04-26 23:21:40 +1000

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={barlow.variable}>{children}</body>
+      <body className={barlow.variable}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
