24bb2954073a9cc3278e139a1f76980b98981965
Matthew Yau <myau0005@student.monash.edu>
2025-05-14 21:01:52 +1000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
-import { FileText, BookOpen } from "lucide-react";
+import { FileText } from "lucide-react";
 import Link from "next/link";
 import DogWithParticles from "@/components/dawg";
 
